0fdc10c1038dcdd72545c57c23e342ae5e97ac5a
Jason Verber <jason.verber@wgu.edu>
2018-07-25 10:56:39 -0500

    Swapped out inline styling for classes. Custom eval handling and labels.

--- src/docs/index.jsx
+++ src/docs/index.jsx
@@ -3,19 +3,65 @@ import { render } from "react-dom";
 import {Quiz} from "../../lib";
 import "./styles.css";
 
+const evalHandler = (question) => {
+  question.submitted=true;
+  return question;
+};
+
+const outcomeHandler = (questions) => {
+  if (questions[0].answers[0].selected && questions[1].answers[0].selected && questions[2].answers[1].selected) return <p>You're an exclamation point!</p>;
+  if (questions[1].answers[1].selected && questions[2].answers[1].selected) return <p>You're an em-dash!</p>;
+  if (questions[0].answers[1].selected && questions[1].answers[0].selected) return <p>You're a question mark!</p>
+  return <p>You're a semi-colon!</p>;
+}
+
 function Demo() {
   return (
     <div>
-      <h1>Demo</h1>
-      <Quiz questionsPerPage='all' questions={[
-        {
-          prompt: 'What is the capital of Germany?',
-          answers: ['Frankfurt','Bonn',{text: 'Berlin', correct:true},'Munich']
-        },
-        {
-          prompt: 'What was the capital of West Germany?',
-          answers: ['Frankfurt',{text:'Bonn', correct:true},{text: 'Berlin', correct:false},'Munich']
-        }]} />
+      <h1>Demo Graded Quiz</h1>
+      <Quiz header=<h2>[[title]]</h2>
+            footer={(state)=>state.currentPage>-1?<p>Score: [[score]] - Page: [[currentPage]]</p>:''}
+            title='Geography Quiz'
+            questionsPerPage='all'
+            questions={[
+                        {
+                          prompt: 'What is the capital of Germany?',
+                          answers: ['Frankfurt','Bonn',{text: 'Berlin', correct:true},'Munich']
+                        },
+                        {
+                          prompt: 'What was the capital of West Germany?',
+                          answers: ['Frankfurt',{text:'Bonn', correct:true},{text: 'Berlin', correct:false},'Munich']
+                        },{
+                          prompt: 'Which cities have been the capital of the Federal Republic of Germany?',
+                          answers: ['Frankfurt',{text:'Bonn', correct:true, points:1},{text: 'Berlin', correct:true, points:1},'Munich'],
+                          multiSelect:'all'
+                        }]} />
+
+
+      <h1>Demo Ungraded Quiz</h1>
+      <Quiz header=<h2>[[title]]</h2>
+            footer={(state)=>state.currentPage>-1?<p>Page: [[currentPage]]</p>:''}
+            title='Which Punctuation Mark Are You?'
+            questionsPerPage={1}
+            evalHandler={evalHandler}
+            outcomeHandler={outcomeHandler}
+            finishMessage='And the results are in...'
+            hideEndEval={true}
+            score={false}
+            className='SurveyMaster'
+            questions={[
+                        {
+                          prompt: 'What is your outlook?',
+                          answers: ['Optimistic','Pessimistic']
+                        },
+                        {
+                          prompt: 'Does your life have a point?',
+                          answers: ['Yes','No']
+                        },{
+                          prompt: 'Do people talk over you, or give you space in a conversation?',
+                          answers: ['Talk over.','Give space.','It depends.']
+                        }]} />
+
     </div>
   );
 }
--- src/lib/Quiz.jsx
+++ src/lib/Quiz.jsx
@@ -4,12 +4,14 @@ import React, { Component } from "react";
     const handleClick = () => {
       props.handleClick(props.index, props.question);
     }
-    const style = () => {
-      if (props.selected) return {fontWeight:'bold'};
-      return {};
+    const className = () => {
+      let name = props.className+'Answer ';
+      if (props.selected) name+= props.className+'Selected ';
+      if (props.classes) name+= props.className+props.classes.join(' '+props.className)+' ';
+      return name;
     }
     return (
-      <li onClick={handleClick} style={Object.assign(style(),props.style||{})}>{props.text}</li>
+      <li onClick={handleClick} className={className()}>{props.text}</li>
     )
   };
 
@@ -17,7 +19,7 @@ class Quiz extends Component {
   constructor(props) {
     super(props);
 
-    let {questions, questionsPerPage, currentPage, inProgress, beforeStart, afterStart, score, penalty, autoEval, className, ...userProps} = this.props;
+    let {questions, questionsPerPage, currentPage, inProgress, beforeStart, afterStart, score, penalty, autoEval, endEval, hideEndEval, className, header, footer, beginButton, evaluateButton, continueButton, finishButton, startMessage, finishMessage, pageLabel, scoreLabel, evalHandler, outcomeHandler, ...userProps} = this.props;
 
     this.state = {
       questions: this.prepareQuestions(questions),
@@ -26,11 +28,25 @@ class Quiz extends Component {
       inProgress: currentPage!==undefined && currentPage>-1 ? true : (inProgress || false),
       beforeStart,
       afterStart,
-      score: score || 0,
+      score: score!==undefined ? score : 0,
       penalty: penalty || 0,
-      autoEval: autoEval || true,
+      autoEval: autoEval || false,
+      endEval: endEval!==undefined ? endEval : true,
+      hideEndEval: hideEndEval || false,
       className: className || 'QuizMaster',
-      userProps: userProps
+      userProps: userProps,
+      header,
+      footer,
+      beginButton: beginButton || 'Begin',
+      evaluateButton: evaluateButton || 'Evaluate',
+      continueButton: continueButton || 'Continue',
+      finishButton: finishButton || 'Finish',
+      startMessage: startMessage || 'Click to begin!',
+      finishMessage: finishMessage || 'Quiz complete!',
+      pageLabel : pageLabel || 'Page: ',
+      scoreLabel : scoreLabel || 'Score: ',
+      evalHandler,
+      outcomeHandler
     }
   }
 
@@ -52,8 +68,9 @@ class Quiz extends Component {
   };
 
   finishQuiz = () => {
-    if (this.state.beforeFinish) this.state.beforeFinish();
-    this.setState({inProgress:false, currentPage:-2, finished:true}, this.state.afterFinish||null);
+    let {endEval} = this.state;
+    this.setState({inProgress:false, currentPage:-2, finished:true}, endEval ? this.evaluate(-1) : null );
+    this.state.finishCallback ? this.state.finishCallback() : null;
   };
 
   setPage = (newPage) => {
@@ -71,6 +88,7 @@ class Quiz extends Component {
     let {questions, multiSelect, autoEval} = this.state;
     if (questions[qIndex].submitted) return;
     questions[qIndex].answers = questions[qIndex].answers.map((answer, i)=>{
+      multiSelect = questions[qIndex].multiSelect!==undefined ? questions[qIndex].multiSelect : multiSelect;
       answer.selected = i===aIndex ? !answer.selected : (multiSelect?answer.selected:false);
       return answer;
     });
@@ -78,22 +96,27 @@ class Quiz extends Component {
   };
 
   evaluate = (index) => {
-    let {questions, score, penalty} = this.state;
-    questions = questions.map((question, i) => {
-      if (!question.displayed || question.submitted || (index!==undefined && i!==index)) return question;
+    let {questions, score, penalty, multiSelect, evalHandler} = this.state;
+    questions = questions.map(evalHandler?evalHandler:(question, i) => {
+      let displayed = index>-1 ? question.displayed : true;
+      let indexed = typeof index !== 'number' || index === -1 || i === index;
+      if (!displayed || question.submitted || !indexed) return question;
       question.attempts = question.attempts ? question.attempts-1 : 0;
       let selected = question.answers.filter(answer=>answer.selected);
       let correct = question.answers.filter(answer=>answer.correct);
-      if (selected.find(answer=>answer.correct)) question.correct = true;
+      multiSelect = question.multiSelect!==undefined ? question.multiSelect : multiSelect;
+      if (multiSelect!=='all' && selected.find(answer=>answer.correct)) question.correct = true;
+      if (multiSelect==='all' && selected.every(answer=>answer.correct) && correct.every(answer=>answer.selected)) question.correct = true;
       if (question.attempts===0 || question.correct === true) question.submitted = true;
       if (question.correct) {
-        question.pointsEarned = questions.possiblePoints || 1;
+        question.pointsEarned = selected.reduce((a,b)=>b.correct&&b.points?a+b.points:a,0) || question.possiblePoints || 1;
         score += question.pointsEarned;
       } else {
         if (penalty) score -= penalty;
       }
       if (question.submitted) question.answers.map(answer=>{
-        answer.style = answer.correct ? {color:'green', fontWeight:'bold'} : (answer.selected?{color:'red'}:{});
+        answer.classes = answer.classes || [];
+        answer.classes.push(answer.correct ? (answer.selected?'CorrectAnswer':'MissingAnswer') : (answer.selected?'IncorrectAnswer':''))
         return answer;
       });
       return question;
@@ -104,34 +127,34 @@ class Quiz extends Component {
   setDisplayedQuestions = () => {
     let {questions, questionsPerPage, currentPage} = this.state;
     let start = currentPage * questionsPerPage;
-    let end = start+questionsPerPage>questions.length ? start+questions.length%questionsPerPage : start+questionsPerPage; console.log("end "+end)
+    let end = start+questionsPerPage>questions.length ? start+questions.length%questionsPerPage : start+questionsPerPage;
     questions = questions.map((question, i)=>{
       question.displayed = (i>=start && i<end);
       return question;
     });
-    console.log(questions);
     this.setState({questions});
   };
 
   renderQuizQuestions = () => {
-    let {questions, currentPage, questionsPerPage, autoEval} = this.state;
+    let {questions, currentPage, questionsPerPage, autoEval, endEval, finished, className, evaluateButton, continueButton, finishButton} = this.state;
+    let totalQuestions = questions.length;
     let unsubmitted = questions.filter(question=>question.displayed && !question.submitted).length;
     let unanswered = questions.filter(question=>question.displayed && question.answers.filter(answer=>answer.selected).length===0).length;
-    let finished = questions.every(question=>question.submitted);
+    let finishedPage = questions.every(question=>question.submitted);
     questions = questions.filter(question=>question.displayed).map((question, i)=>(
-      <div key={i}>
+      <div className={className+'Question'+(finished?' '+className+(question.correct?'Correct':'Incorrect'):'')} key={i}>
         <span>{question.prompt}</span>
         <ul>
           {question.answers.map((answer, j)=>(
-            <QuizAnswer key={j} index={j} question={questionsPerPage*currentPage+i} text={answer.text} selected={answer.selected} style={answer.style} handleClick={this.handleAnswerClick} />
+            <QuizAnswer key={j} index={j} question={questionsPerPage*currentPage+i} text={answer.text} selected={answer.selected} submitted={question.submitted} className={className} classes={answer.classes} handleClick={this.handleAnswerClick} />
           ))}
         </ul>
       </div>
     ));
 
-    if (unsubmitted>0 && !autoEval) questions.push(<div key='evalButton'><button disabled={unanswered>0} onClick={this.evaluate}>Evaluate</button></div>);
-    if (!finished && unsubmitted===0) questions.push(<div key='continueButton'><button onClick={this.nextPage}>Continue</button></div>);
-    if (finished) questions.push(<div key='finishButton'><button onClick={this.finishQuiz}>Finish</button></div>);
+    if (!finished && unsubmitted>0 && !autoEval && !endEval) questions.push(<div key='evalButton'><button disabled={unanswered>0} onClick={this.evaluate}>{evaluateButton}</button></div>);
+    if (!finishedPage && (unsubmitted===0 || (endEval && (currentPage+1)*questionsPerPage<totalQuestions))) questions.push(<div key='continueButton'><button disabled={unanswered>0} onClick={this.nextPage}>{continueButton}</button></div>);
+    if (!finished && (finishedPage || (endEval && (currentPage+1)*questionsPerPage>=totalQuestions))) questions.push(<div key='finishButton'><button disabled={unanswered>0} onClick={this.finishQuiz}>{finishButton}</button></div>);
     return questions;
   };
 
@@ -140,21 +163,30 @@ class Quiz extends Component {
   };
 
   renderQuizStart = () => {
-    let {startMessage} = this.state;
+    let {startMessage, beginButton} = this.state;
     return (
       <>
-      <p><span>{startMessage || 'Click to begin!'}</span></p>
-      <p><button onClick={this.startQuiz}>Begin</button></p>
+      <p><span>{startMessage}</span></p>
+      <p><button onClick={this.startQuiz}>{beginButton}</button></p>
       </>
     );
   }
 
   renderQuizFinish = () => {
-    let {finishMessage, score} = this.state;
+    let {finishMessage, score, questions, endEval, hideEndEval, className, outcomeHandler} = this.state;
+    questions = questions.map(question=>{question.displayed=true; return question});
     return (
       <>
-      <p><span>{finishMessage || 'Quiz complete!'}</span></p>
-      <p>Overall Score: {score}</p>
+      <p><span>{finishMessage}</span></p>
+      {endEval && !hideEndEval?(
+        <div className={className+'EndEval'}>
+        {this.renderQuizQuestions()}
+        </div>
+      ):('')}
+      {outcomeHandler ? outcomeHandler(questions) : ''}
+      {score!==false?(
+        <p>Overall Score: {score}</p>
+      ):('')}
       </>
     );
   }
@@ -167,22 +199,31 @@ class Quiz extends Component {
   };
 
   renderQuizHeader = () => {
-    let {score, questions, currentPage, inProgress, finished} = this.state;
-    if (this.props.header) return this.props.header;
+    let {score, questions, currentPage, inProgress, finished, pageLabel} = this.state;
+    if (this.props.header) return this.renderUserString(typeof this.props.header === 'function' ? this.props.header(this.state) : this.props.header);
     return (
-      <span>{inProgress&&!finished?'Page: '+(currentPage+1):''}</span>
+      <span>{inProgress&&!finished?pageLabel+(currentPage+1):''}</span>
     )
   };
 
   renderQuizFooter = () => {
-    let {score, questions, inProgress, finished} = this.state;
+    let {score, questions, inProgress, finished, scoreLabel} = this.state;
+    if (this.props.footer) return this.renderUserString(typeof this.props.footer === 'function' ? this.props.footer(this.state) : this.props.footer);
     return (
-      <span>{inProgress&&!finished?'Score: '+score:''}</span>
+      <span>{inProgress&&!finished?scoreLabel+score:''}</span>
     )
   };
 
   renderUserString = (string) => {
-
+    if (typeof string !== 'string') {
+      string = React.cloneElement(string, {children:this.renderUserString(string.props.children)});
+      return string;
+    }
+    let vars = Object.assign({},this.state);
+    vars.currentPage = vars.currentPage+1;
+    vars.totalQuestions = vars.questions.length;
+    string = string.replace(/\[\[/g,"{").replace(/\]\]/g,"}");
+    return string.replace(/\{(\w+)\}/g,(_,k)=>vars[k]!==undefined?vars[k]:vars.userProps[k]||'');
   }
 
   render = () => {
